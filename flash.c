69d2d33b50bd9ad6dfedf037f27b16014045bbc4
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2020-10-19 11:15:31 +0200

    [DOC] SEOS-1701: fix error string to point to an hanbook section rather than a wiki link

--- flash.c
+++ flash.c
@@ -1,1 +1,1 @@
-#error "please overwrite me with as specified in https://wiki.hensoldt-cyber.systems/pages/viewpage.action?pageId=37454004 , 'Flasher Tool for RasPi 3B+'"
+#error "Please overwrite me as specified in Trentos-M handbook, section 'Flasher Tool for RasPi 3B+'."
